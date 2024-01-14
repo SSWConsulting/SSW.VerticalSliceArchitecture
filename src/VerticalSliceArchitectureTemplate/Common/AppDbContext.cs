81b56acca3fbda8c775043e81b43f6a6461babf7
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2024-01-15 09:18:06 +1000

    🐛 Fix EF configs not applying

--- src/VerticalSliceArchitectureTemplate/Common/AppDbContext.cs
+++ src/VerticalSliceArchitectureTemplate/Common/AppDbContext.cs
@@ -3,4 +3,9 @@
 public partial class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
 }
