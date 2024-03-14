690ab31644b6cb5e1e9ff518e2d22d9666ae3f0b
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2024-03-14 22:41:08 +1000

    🧪 Split into minimally passing tests

--- tests/VerticalSliceArchitectureTemplate.Unit.Tests/Features/Todos/Models/TodoTests.cs
+++ tests/VerticalSliceArchitectureTemplate.Unit.Tests/Features/Todos/Models/TodoTests.cs
@@ -7,7 +7,7 @@ namespace VerticalSliceArchitectureTemplate.Unit.Tests.Features.Todos.Models;
 public class TodoTests
 {
     [Fact]
-    public void Todo_Complete_ShouldUpdateCompletedAndAddEvent()
+    public void Todo_Complete_ShouldUpdateCompleted()
     {
         // Arrange
         var item = new Todo
@@ -21,6 +21,22 @@ public class TodoTests
         
         // Assert
         item.Completed.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Todo_Complete_ShouldAddEvent()
+    {
+        // Arrange
+        var item = new Todo
+        {
+            Id = Guid.NewGuid(),
+            Text = "My todo item"
+        };
+        
+        // Act
+        item.Complete();
+        
+        // Assert
         item.StagedEvents.Should().ContainSingle(x => x is TodoCompletedEvent, "because the item was completed");
     }
 }
