f5a58b279f4693223bcd017a4f8ba75c83c0e213
Sam Maher <samsmaher@gmail.com>
2025-05-12 14:16:42 +1000

    #105 Added Heroes endpoints

---
+++ src/VerticalSliceArchitectureTemplate/Common/Extensions/CustomResult.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace VerticalSliceArchitectureTemplate.Common.Extensions;
+
+public static class CustomResult
+{
+    public static IResult Problem(List<Error> errors)
+    {
+        if (errors.Count is 0)
+        {
+            return TypedResults.Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Problem(errors[0]);
+    }
+
+    private static ProblemHttpResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return TypedResults.Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private static ValidationProblem ValidationProblem(List<Error> errors)
+    {
+        var validationErrors = new Dictionary<string, string[]>();
+        foreach (var e in errors)
+        {
+            if (validationErrors.Remove(e.Code, out var value))
+                validationErrors.Add(e.Code, [.. value, e.Description]);
+            else
+                validationErrors.Add(e.Code, [e.Description]);
+        }
+
+        return TypedResults.ValidationProblem(validationErrors, title: "One or more validation errors occurred.");
+    }
+}
---
+++ src/VerticalSliceArchitectureTemplate/Features/Teams/Commands/CreateTeamCommand.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using VerticalSliceArchitectureTemplate.Common.Domain.Teams;
+using VerticalSliceArchitectureTemplate.Common.Extensions;
+
+namespace VerticalSliceArchitectureTemplate.Features.Teams.Commands;
+
+public static class CreateTeamCommand
+{
+    public record Request(string Name) : IRequest<ErrorOr<Success>>;
+    
+    public class Endpoint : IEndpoint
+    {
+        public static void MapEndpoint(IEndpointRouteBuilder endpoints)
+        {
+            endpoints
+                .MapApiGroup(TeamsFeature.FeatureName)
+                .MapPost("/",
+                    async (ISender sender, Request command, CancellationToken ct) =>
+                    {
+                        var result = await sender.Send(command, ct);
+                        return result.Match(_ => TypedResults.Created(), CustomResult.Problem);
+                    })
+                .WithName("CreateTeam")
+                .ProducesPost();
+        }
+    }
+    
+    internal sealed class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty();
+        }
+    }
+    
+    internal sealed class Handler(AppDbContext dbContext)
+        : IRequestHandler<Request, ErrorOr<Success>>
+    {
+        public async Task<ErrorOr<Success>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var team = Team.Create(request.Name);
+
+            await dbContext.Teams.AddAsync(team, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new Success();
+        }
+    }
+}
