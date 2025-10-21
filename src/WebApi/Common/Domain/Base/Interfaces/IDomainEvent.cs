9369c7ac4b648c97ae1f2045f051e0c8a3540acc
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2025-10-21 19:53:33 +1000

    ♻️ Remove MediatR package references from project files

--- src/WebApi/Common/Domain/Base/Interfaces/IDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SSW.VerticalSliceArchitecture.Common.Domain.Base.Interfaces;
-
-/// <summary>
-/// Can be raised by an AggregateRoot to notify subscribers of a domain event.
-/// </summary>
-// public interface IDomainEvent : IEvent;
-
-// NOTE: With FastEndoints, the events must implement IEvent directly.
